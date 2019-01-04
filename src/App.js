e27452af47c10f06afc1d5a6b94b4e589c7cb899
Tom Hodson <tomhodson91@hotmail.com>
2019-01-04 20:22:00 +0000

    Added sliders and programme slots #2

--- src/App.js
+++ src/App.js
@@ -3,6 +3,28 @@ import skyLogo from './Images/skyLogo.png';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      val: 50,
+      val2: 50,
+      val3: 50,
+      val4: 50
+    }
+  };
+
+  updateVal(event) {
+    this.setState({val: event.target.value});
+  }
+  updateVal2(event) {
+    this.setState({val2: event.target.value});
+  }
+  updateVal3(event) {
+    this.setState({val3: event.target.value});
+  }
+  updateVal4(event) {
+    this.setState({val4: event.target.value});
+  }
   render() {
     return (
       <div className="App">
@@ -12,7 +34,7 @@ class App extends Component {
           </div>
           <div className="headers-container">
             <div className="title-bar">
-              I am the title bar
+              Welcome to Moodslider!
             </div>
             <div className="nav-bar">
               I am the nav bar
@@ -21,7 +43,50 @@ class App extends Component {
         </header>
         <div className="inner-wrapper">
           <main>
-            <h1>This will be the body of Moodslider</h1>
+            <div className="sliders-container">
+              <div className="calm-container">
+                <h1 className="emotion">Agitated</h1>
+                <input type="range" min="1" max="100" value={this.state.val} onChange={this.updateVal.bind(this)} class="slider" id="myRange"/>
+                <h1 className="emotion">Calm</h1>
+              </div>
+              <div className="sad-container">
+                <h1 className="emotion">Happy</h1>
+                <input type="range" min="1" max="100" value={this.state.val2} onChange={this.updateVal2.bind(this)} class="slider" id="myRange"/>
+                <h1 className="emotion">Sad</h1>
+              </div>
+              <div className="wide-awake-container">
+                <h1 className="emotion">Tired</h1>
+                <input type="range" min="1" max="100" value={this.state.val3} onChange={this.updateVal3.bind(this)} class="slider" id="myRange"/>
+                <h1 className="emotion">Wide Awake</h1>
+              </div>
+              <div className="fearless-container">
+                <h1 className="emotion">Scared</h1>
+                <input type="range" min="1" max="100" value={this.state.val4} onChange={this.updateVal4.bind(this)} class="slider" id="myRange"/>
+                <h1 className="emotion">Fearless</h1>
+              </div>
+            </div>
+            <div className="programmes-container">
+              <div className="programme1-container">
+                <div className="programme1">No Content</div>
+                <div className="programme1-title">No Content</div>
+              </div>
+              <div className="programme2-container">
+                <div className="programme2">No Content</div>
+                <div className="programme1-title">No Content</div>
+              </div>
+              <div className="programme3-container">
+                <div className="programme3">No Content</div>
+                <div className="programme1-title">No Content</div>
+              </div>
+              <div className="programme4-container">
+                <div className="programme4">No Content</div>
+                <div className="programme1-title">No Content</div>
+              </div>
+              <div className="programme5-container">
+                <div className="programme5">No Content</div>
+                <div className="programme1-title">No Content</div>
+              </div>
+            </div>
           </main>
         </div>
         <footer className="App-footer">
