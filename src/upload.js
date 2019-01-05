d3972f70aae7f173eaaff85ecc868879028dd4c4
Tom Hodson <tomhodson91@hotmail.com>
2019-01-05 16:29:55 +0000

    Upload and read XML file #4

--- src/upload.js
+++ src/upload.js
@@ -1,7 +1,36 @@
 import React from 'react';
 
-export default function home() {
-    return (
-        <h1>This is the uploads page - Upload content here!</h1>
-    );
+class upload extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {foo: 'No content uploaded yet'};
+    }
+
+    onChange(event) {
+        let files = event.target.files;
+        const reader = new FileReader();
+        reader.onload = () => {
+            let res = reader.result;
+            var parser = new DOMParser(), xmlDoc = parser.parseFromString(res, "text/xml");
+            console.log(xmlDoc)
+            this.setState({foo: 'Upload successful!'});
+        }
+        reader.readAsText(files[0])
+    }
+
+    render() {
+        return (
+            <form>
+                <label>
+                    Upload file:
+                    <input type="file" accept=".xml" name="file" onChange={(event) => this.onChange(event)}/>
+                </label>
+                <br />
+                <div>Upload status: {this.state.foo}</div>
+            </form>
+        )
+    }
 }
+
+export default upload;
